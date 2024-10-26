165672ee0399d96889cc5f742a026630d33fe72d
Raj Kapadia <kapadiaraj03@gmail.com>
2024-10-26 19:11:32 -0400

    @Rajk7513

--- User/user_station_selector.py
+++ User/user_station_selector.py
@@ -1,9 +1,6 @@
 #input should be location of getting on and off and time, and output should where to stand to get in the train depending on where the capcity is lower
+from datetime import datetime
 
-station_on_name = print("Which station are u getting on from:" + str(input()))
-station_off_name = print("What is your desired destination:" + str(input()))
-
-
-if (station_on_name and station_off_name ) in StationList.json:
-    #we get the train schedule of all the trains that will be departing from there.
-    
+boarding_station = str(input("Enter your boarding station: "))
+destination_station = str(input("Enter your destination station: "))
+boarding_time = int(input("Enter your boarding time (HH:MM): "))
---
+++ train_related/compartment.py
@@ -0,0 +1,15 @@
+from user import user_station_selector.py
+
+def get_suggested_compartment(boarding_station, destination_station, time):
+    # Find the appropriate train for the given boarding time and stations
+    for train, schedule in station_schedule.items():
+        stations = [s["station"] for s in schedule]
+        if boarding_station in stations and destination_station in stations:
+            # Get the time details for the boarding station
+            station_info = next((s for s in schedule if s["station"] == boarding_station), None)
+            if station_info and time_in_range(time, station_info["arrival"], station_info["departure"]):
+                # Suggest the compartment with the lowest capacity
+                compartments = train_stop_list[train]["compartments"]
+                best_compartment = min(compartments, key=lambda x: x["capacity"])
+                return f"Board Train {train} at {boarding_station}. Stand near Compartment {best_compartment['compartment']} (Capacity: {best_compartment['capacity']})."
+    return "No suitable train found for the given time and stations."
